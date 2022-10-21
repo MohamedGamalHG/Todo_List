41a96541a78a57e484e408ba03e6fe2c730b0113
MohamedGamalHG <49466286+MohamedGamalHG@users.noreply.github.com>
2022-10-21 20:57:51 +0200

    Update TodoListController.php

--- app/Http/Controllers/Todo/TodoListController.php
+++ app/Http/Controllers/Todo/TodoListController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
+    
+    /* using Repository Desgin Pattern for the blow code */
     protected $todo;
     public function __construct(TodoListRepositoryInterface $todo)
     {
@@ -21,22 +23,7 @@ class TodoListController extends Controller
         return $this->todo->index();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(TodoRequest $request)
     {
         return $this->todo->store($request);
@@ -48,30 +35,14 @@ class TodoListController extends Controller
         return $this->todo->update($request);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(Request $request)
     {
         return $this->todo->delete($request);
     }
-
+    
     public function Check_Status(Request $request)
     {
-        $todo = Todo::findOrFail($request->id);
-        if($todo){
-            $todo->update(['status' => 1]);
-            return response()->json([
-                'status'       => true,
-                'msg'          => 'data complete',
-                'id'           => $request->id
-            ]);
-        }else return response()->json([
-            'status'       => false,
-            'msg'          => 'data note saved',
-        ]);
+       return $this->todo->Check_Status($request);
     }
 }
