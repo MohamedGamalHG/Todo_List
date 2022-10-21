28eef1350898fe27892173805fedf06c5f7506a7
MohamedGamalHG <49466286+MohamedGamalHG@users.noreply.github.com>
2022-10-21 21:01:12 +0200

    Update TodoListRepository.php

--- app/Repository/TodoListRepository.php
+++ app/Repository/TodoListRepository.php
@@ -54,6 +54,23 @@ class TodoListRepository implements TodoListRepositoryInterface
         return redirect()->route('todo.index');
 
     }
+    /* this function check status where 1 is complete task and 0 others using jquery ajax */
+    public function Check_Status($request)
+    {
+        
+         $todo = Todo::findOrFail($request->id);
+                if($todo){
+                    $todo->update(['status' => 1]);
+                    return response()->json([
+                        'status'       => true,
+                        'msg'          => 'data complete',
+                        'id'           => $request->id
+                    ]);
+                }else return response()->json([
+                    'status'       => false,
+                    'msg'          => 'data note saved',
+                ]);
+    }
 
 
 }
